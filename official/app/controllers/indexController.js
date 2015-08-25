41a314e3018ef1ba679e9e0582808ecd1765a790
RTBathula <battu.network@gmail.com>
2015-08-26 00:28:27 +0530

    updated

--- official/app/controllers/indexController.js
+++ official/app/controllers/indexController.js
@@ -1,9 +1,9 @@
 app.controller('indexController',
-function ($scope,$rootScope,$location) {
+function ($scope,$rootScope,$location,$state) {
 
 $scope.logOut=function(){
 	$.removeCookie('officialUser', { path: '/' });
-	window.location.href="http://localhost:1440/official/accounts"; 
+	window.location.href=domain+"/official/accounts"; 
 };
 
 $scope.$watch(function(scope) {
@@ -12,10 +12,27 @@ $scope.$watch(function(scope) {
 },function(newPath,oldPath) {
 
     if(!$.cookie('officialUser') || $.cookie('officialUser')=="null" || $.cookie('officialUser')=="undefined"){          
-     	window.location.href="http://localhost:1440/official/accounts";
+     	window.location.href=domain+"/official/accounts";
     }else if($.cookie('officialUser')){
 		var obj=JSON.parse($.cookie('officialUser'));
 		$rootScope.officialUser=CB.fromJSON(obj.document);
+
+		var isDataEntry=$rootScope.officialUser.get("isDataEntry");
+		var isCallOperator=$rootScope.officialUser.get("isCallOperator");
+
+		var strArray=newPath.split("/");
+		if(isDataEntry && strArray[1]=="call"){
+			window.location.href=domain+"/official/#/data/view";
+			//$state.transitionTo('dataOperatorView');
+			//$state.go('dataOperatorView');
+			//$scope.$digest() 
+			 
+		}else if(isCallOperator && strArray[1]=="data"){
+			window.location.href=domain+"/official/#/call/search"; 
+			//$state.transitionTo('callOperatorSearch');
+			//$state.transitionTo('callOperatorSearch');
+			//$scope.$digest()
+		} 
 	}   
 
 });
